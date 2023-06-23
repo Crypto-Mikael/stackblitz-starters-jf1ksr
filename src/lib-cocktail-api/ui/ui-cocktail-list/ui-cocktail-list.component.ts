a2adb91fb7ec602879abf2cb00da400014339a56
Crypto-Mikael <80548535+Crypto-Mikael@users.noreply.github.com>
2023-06-23 19:34:19 -0300

    1 file modified

--- src/lib-cocktail-api/ui/ui-cocktail-list/ui-cocktail-list.component.ts
+++ src/lib-cocktail-api/ui/ui-cocktail-list/ui-cocktail-list.component.ts
@@ -1,21 +1,36 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, FormsModule } from '@angular/forms';
 import { MatListModule } from '@angular/material/list';
 import { CocktailsDTO } from '../../models/DTOs/CocktailslDTO';
+
+type FormGroupCocktail = FormGroup<{
+  nameCocktail: FormControl<string | null>;
+  tagsCocktails: FormControl<string[] | null>;
+}>;
+
 @Component({
   selector: 'app-ui-cocktail-list',
   templateUrl: './ui-cocktail-list.component.html',
   styleUrls: ['./ui-cocktail-list.component.css'],
   standalone: true,
-  imports: [CommonModule, MatListModule],
+  imports: [CommonModule, MatListModule, FormsModule],
 })
 export class UiCocktailListComponent implements OnInit {
-  _cocktais: CocktailsDTO[] = [];
-  
+  _cocktais = new FormArray<FormGroupCocktail>([]);
+
   @Input() set cocktais(cocktais: CocktailsDTO[]) {
-    this._cocktais = cocktais.filter(({ idDrink }) => idDrink === '11007');
+    cocktais.forEach((cocktail) => {});
   }
   constructor() {}
+
+  addInFormArray(nameCocktail: string, tagsCocktails: string[]) {
+    const formGroup = new FormGroup({
+      nameCocktail: new FormControl<string | null>(nameCocktail),
+      tagsCocktails: new FormControl<string[] | null>(tagsCocktails),
+    }) as FormGroupCocktail;
+    this._cocktais.push(formGroup);
+  }
 
   ngOnInit() {}
 }
