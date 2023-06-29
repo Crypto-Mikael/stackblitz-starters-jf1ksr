6af292fec952d5e665abf4193999893e73da4f3f
Crypto-Mikael <80548535+Crypto-Mikael@users.noreply.github.com>
2023-06-29 14:05:55 -0300

    5 files modified

--- src/lib-cocktail-api/features/feature-cocktail-form/feature-cocktail-form.component.ts
+++ src/lib-cocktail-api/features/feature-cocktail-form/feature-cocktail-form.component.ts
@@ -1,15 +1,53 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import {
+  FormArray,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+
+
+//
+
+interface CocktailGroup {
+  cocktailName: FormControl<string | null>;
+  cocktailsQuantity: FormControl<number | null>;
+}
 
 @Component({
-  selector: 'app-feature-cocktail-form',
+  selector: 'feature-cocktail-form',
   templateUrl: './feature-cocktail-form.component.html',
   styleUrls: ['./feature-cocktail-form.component.css'],
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
 })
-export class FeatureCocktailFormComponent implements OnInit {
-  constructor() {}
+export class FeatureCocktailFormComponent impl {
+  public cocktailsOnDay = new FormArray<FormGroup<CocktailGroup>>([]);
 
-  ngOnInit() {}
+  public newCocktailFormGroup(
+    cocktailNameValue: string | null,
+    cocktailsQuantityValue: number | null
+  ): FormGroup<CocktailGroup> {
+    const newFormGroupCocktail = new FormGroup<CocktailGroup>({
+      cocktailName: new FormControl<string | null>(cocktailNameValue),
+      cocktailsQuantity: new FormControl<number | null>(cocktailsQuantityValue),
+    });
+    return newFormGroupCocktail;
+  }
+
+  public addCocktailOnDayFormArray = (
+    formGroup: FormGroup<CocktailGroup>
+  ): void => this.cocktailsOnDay.push(formGroup);
 }
--- src/lib-cocktail-api/ui/ui-final-de-semana-stepper/ui-final-de-semana-stepper.component.ts
+++ src/lib-cocktail-api/ui/ui-final-de-semana-stepper/ui-final-de-semana-stepper.component.ts
@@ -1,11 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormArray,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatStepperModule } from '@angular/material/stepper';
 import { MatTabsModule } from '@angular/material/tabs';
-
+import { FeatureCocktailFormComponent } from '../../features/feature-cocktail-form/feature-cocktail-form.component';
 @Component({
   selector: 'ui-final-de-semana-stepper',
   templateUrl: './ui-final-de-semana-stepper.component.html',
@@ -15,23 +20,15 @@ import { MatTabsModule } from '@angular/material/tabs';
     CommonModule,
     MatTabsModule,
     ReactiveFormsModule,
+    FeatureCocktailFormComponent,
     MatFormFieldModule,
     MatStepperModule,
     MatInputModule,
   ],
 })
 export class UiFinalDeSemanaStepperComponent implements OnInit {
-  
-  sabado = new FormGroup({
-    cocktail: new FormControl<string | null>(null),
-    quantidade: new FormControl<number | null>(null),
-  });
-
-  domingo = new FormGroup({
-    cocktail: new FormControl<string>(''),
-    quantidade: new FormControl<number>(0),
-  });
   constructor() {}
+  public formGeral: string | null = null;
 
   ngOnInit() {}
 }
